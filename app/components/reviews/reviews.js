20977362198976e2c6c92780be6bd970f2ebb55a
Kirill Kuleshov <nstkuleshov1@gmail.com>
2019-05-30 23:56:18 +0300

    reviews

--- app/components/reviews/reviews.js
+++ app/components/reviews/reviews.js
@@ -1,4 +1,29 @@
 $('.reviews__list').slick({
     slidesToShow: 2,
-    variableWidth: true
+    variableWidth: true,
+    prevArrow: $('.reviews__prev'),
+    nextArrow: $('.reviews__next'),
+    adaptiveHeight: true,
+    responsive: [
+        {
+            breakpoint: 1023,
+            settings: {
+                slidesToShow: 1,
+                centerMode: true
+            }
+        }
+    ]
+});
+
+let leftBtn = $(".reviews__curr-slide");
+let rightBtn = $(".reviews__total-slide");
+
+$(".reviews__list").on("init", function(event, slick){
+    $(leftBtn).text(String('0' + parseInt(slick.currentSlide + 1) + ''));
+    $(rightBtn).text(String('/0' + parseInt(slick.slideCount)));
+});
+
+$(".reviews__list").on("afterChange", function(event, slick, currentSlide){
+    $(leftBtn).text(String('0' + parseInt(slick.currentSlide + 1)) + '');
+    $(rightBtn).text(String('/0' + slick.slideCount));
 });
