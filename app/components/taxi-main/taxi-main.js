6ac6510c9bdc1128a52d4b977ed131bdb370a70c
ValentynKvasnytskyi <kvasnitskiy.v@gmail.com>
2019-06-07 17:55:55 +0300

    taxi

--- app/components/taxi-main/taxi-main.js
+++ app/components/taxi-main/taxi-main.js
@@ -1,10 +1,16 @@
 let taxiBlock = document.querySelectorAll('.taxi-main__list');
 
-
 taxiBlock.forEach((e) => {
-    let taxiSublist = e.querySelector('.taxi-main__sublist');
-    e.addEventListener('click', () => {
+    let openSubBtn = e.querySelector('.taxi-click'),
+        openSubChevron = e.querySelector('i'),
+        taxiSublist = e.querySelector('.taxi-main__sublist');
+
+    openSubBtn.addEventListener('click', () => {
         taxiSublist.classList.toggle('taxi-main__sublist_hidden');
         e.classList.toggle('showed');
-    })
+    });
+    openSubChevron.addEventListener('click', () => {
+        taxiSublist.classList.toggle('taxi-main__sublist_hidden');
+        e.classList.toggle('showed');
+    });
 });
