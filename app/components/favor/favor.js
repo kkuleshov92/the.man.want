7ecb1c75bacee3457f1b04c213a8217fea612735
kkuleshov92 <nstkuleshov1@gmail.com>
2019-06-13 17:41:08 +0300

    edit

--- app/components/favor/favor.js
+++ app/components/favor/favor.js
@@ -1,10 +1,17 @@
 
 $('.favor__item').on({
     mouseenter: function () {
-        $(this).find('.favor__item_hover').fadeIn();
+        $(this).find('.favor__item_hover').fadeIn('fast');
     },
     mouseleave: function () {
-        $(this).find('.favor__item_hover').fadeOut();
+        $(this).find('.favor__item_hover').fadeOut('fast');
     }
 });
 
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 600) {
+        $('.footer__btn-top').css('display','block');
+    } else {
+        $('.footer__btn-top').css('display','none');
+    }
+});
